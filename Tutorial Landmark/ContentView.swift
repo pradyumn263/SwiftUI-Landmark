632d1f4d4d78399d3743e9c9c1356c7fa00075f1
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2020-12-25 14:53:16 +0530

    Initial Commit

---
+++ Tutorial Landmark/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tutorial Landmark
+//
+//  Created by Pradyumn Shukla on 25/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tutorial Landmark/Tutorial_LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tutorial_LandmarkApp.swift
+//  Tutorial Landmark
+//
+//  Created by Pradyumn Shukla on 25/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Tutorial_LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
